95a7493758aae271c381245321c08208e9db58a0
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-08-17 21:44:57 -0300

    feat: create project

---
+++ src/test/java/com/example/unit/tests/SpringTestesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unit.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
